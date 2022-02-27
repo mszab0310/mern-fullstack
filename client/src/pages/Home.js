5fc65f41edfbfa7133cb9a240d67b85101df772f
mszab0310 <mszabi0310@gmail.com>
2022-02-27 11:02:53 +0200

    Styling home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
   const navigate = useNavigate();
   console.log(localStorage.getItem("token"));
   return (
-    <div>
+    <div className="Home">
       <h1>Welcome!</h1>
-      <button type="button" onClick={() => navigate("/login")}>
-        Login
-      </button>
-      <button type="button" onClick={() => navigate("/register")}>
-        Sign Up
-      </button>
+      <div className="buttons">
+        <button type="button" onClick={() => navigate("/login")}>
+          Login
+        </button>
+        <button type="button" onClick={() => navigate("/register")}>
+          Sign Up
+        </button>
+      </div>
     </div>
   );
 }
